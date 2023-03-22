3fc923edd9b97831979a95d0d549a19bbbf136bd
CasEuser1 <113170768+CasEuser1@users.noreply.github.com>
2023-03-22 09:26:07 +0100

    .

--- wihn-template2/buttons-template/buttons.js
+++ wihn-template2/buttons-template/buttons.js
@@ -9,16 +9,16 @@ function changeImage(doosje){
     container.style.backgroundImage = "url(images/bg"+doosje+".jpg)"
     plaatje = document.getElementById('plaatje')
     plaatje.src = "images/"+doosje+".jpg"
-    if (doosje = "1"){
+    if (String(doosje) === "1"){
         button1 += 1
         document.getElementById('1').innerHTML = button1
     }
 
-    else if (doosje = "2"){
+    else if (String(doosje) === "2"){
         button2 += 1
         document.getElementById('2').innerHTML = button2
     }
-    else if (doosje = "3"){
+    else if (String(doosje) === "3"){
         button3 += 1
         document.getElementById('3').innerHTML = button3
     }

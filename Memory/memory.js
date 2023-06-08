b3d3ea4a66bac27ba9d429b99f5819a94b0c21a1
CasEuser1 <113170768+CasEuser1@users.noreply.github.com>
2023-06-08 10:21:56 +0200

    .

--- Memory/memory.js
+++ Memory/memory.js
@@ -1,4 +1,4 @@
-let imagelist = ['images/kirito.jpg','asuna','yuuki','suguha','alice','sinon','bercouli','eiji','yuna','yui']
+let imagelist = ['images/kirito.jpg','images/asuna.jpg','images/yuuki.jpg','images/suguha.jpg','images/alice.jpg','images/sinon.jpg','images/bercouli.jpg','images/eiji.jpg','images/yuna.jpg','images/yui.jpg']
 imagelist = shuffle(imagelist)
 
 function shuffle(array) {
@@ -12,7 +12,31 @@ function shuffle(array) {
     }
     return array;
 }
+// for(x = 0; imagelist < x; x++){
+//     let img = document.createElement("img");
+//     img.src = "images";
+//     let src = document.getElementById("images");
+//     src.appendChild(img);
+// }
 
-let i = document.createElement("IMG");
-i.src = "images/kirito.jpg";
-document.getElementById("body").appendChild(i);
+function functieArray() {
+    var gallery = document.getElementById("pozeGallery");
+    for (i = 0; i < imgArray.length; i++) {
+        gallery.appendChild(imgArray[i]);
+    }
+};
+var imgArray = new Array();
+imgArray[0] = new Image();
+imgArray[0].src = 'images/kirito.jpg';
+
+imgArray[1] = new Image();
+imgArray[1].src = 'images/asuna.jpg';
+
+imgArray[2] = new Image();
+imgArray[2].src = 'img/Gallery/poza2.jpg';
+
+imgArray[3] = new Image();
+imgArray[3].src = 'img/Gallery/poza3.jpg';
+
+imgArray[4] = new Image();
+imgArray[4].src = 'img/Gallery/poza4.jpg';
